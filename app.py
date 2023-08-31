8b032980fbf3e1613b5b13ec9412ac1167ae294f
Ahmed-M1 <88033251+Ahmed-M1@users.noreply.github.com>
2023-08-31 11:12:08 +0500

    Main Commit

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
 
 
-
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    bmi_calculator = BMICalculator()
-    bmi_calculator.show()
-    sys.exit(app.exec_())
+  
