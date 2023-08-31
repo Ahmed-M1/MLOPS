9bf4c01af022e44b6d032e34c6ddca939de93416
Ahmed-M1 <88033251+Ahmed-M1@users.noreply.github.com>
2023-08-31 23:20:28 +0500

    Class initializer

--- app.py
+++ app.py
@@ -1,4 +1,31 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
 
+
+class BMICalculator(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("BMI Calculator")
+        self.setGeometry(300, 300, 300, 200)
+
+        self.weight_label = QLabel("Weight (kg):")
+        self.weight_input = QLineEdit()
+        self.height_label = QLabel("Height (m):")
+        self.height_input = QLineEdit()
+        self.calculate_button = QPushButton("Calculate")
+        self.result_label = QLabel()
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.weight_label)
+        self.layout.addWidget(self.weight_input)
+        self.layout.addWidget(self.height_label)
+        self.layout.addWidget(self.height_input)
+        self.layout.addWidget(self.calculate_button)
+        self.layout.addWidget(self.result_label)
+
+        self.calculate_button.clicked.connect(self.calculate_bmi)
+
+        self.setLayout(self.layout)
+
+
   
